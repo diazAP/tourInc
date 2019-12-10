124d227d305aaaa3b9243ab10cc42fc83d761cf4
diazAP <38515299+diazAP@users.noreply.github.com>
2019-12-10 20:41:59 +0700

    ubah username password db

--- koneksi.php
+++ koneksi.php
@@ -5,7 +5,7 @@
 	#3 password;
 	#4 database;
 
-	$con = mysqli_connect('remotemysql.com', 'q6ZIYLItgn', 'A8s2PsFY60', 'q6ZIYLItgn');
+	$con = mysqli_connect('local', 'uname', 'pw', 'dbname');
 
 
  ?>
